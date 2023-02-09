347a0c06cd3939cb6270025f3a2cfaed9352ad3d
swapnil9096 <swapnilbobade246@gmail.com>
2023-02-09 18:11:02 +0530

    Adding paymennt program

---
+++ Payment.java
@@ -0,0 +1,18 @@
+package pkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Payment {
+	@Test
+	public void verifyPayment()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+	}
+}
